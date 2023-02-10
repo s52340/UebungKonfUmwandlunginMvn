400f7433c8633842362f960ea816ad2e16f766d0
bthaler <bianca.thaler@gmx.net>
2023-02-10 01:34:59 +0100

    nur git actions gepulled

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(){
+        int c = a + b;
+        return c;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    Calculator calculator;
+    @org.junit.jupiter.api.Test
+    void add() {
+        calculator = new Calculator(3,4);
+
+        Assertions.assertEquals(7,calculator.add());
+    }
+
 }
